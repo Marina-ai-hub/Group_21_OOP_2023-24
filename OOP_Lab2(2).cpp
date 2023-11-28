64ed828c83aa5cf23fd31a7e327d34c2d8edfe5d
Marina-ai-hub <150517190+Marina-ai-hub@users.noreply.github.com>
2023-11-28 16:44:00 +0200

    Add files via upload

--- OOP_Lab2(2).cpp
+++ OOP_Lab2(2).cpp
@@ -1,20 +1,123 @@
-// OOP_Lab2(2).cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <string.h>
+#include <algorithm>
+#include <vector>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+#define MAX_LEN 100
+#define MAX_TYPE_LEN 3
+#define MAX_GENRE_LEN 11
+
+class Filters {
+    string type, genre;
+    // int GenreSelected, TypeSelected;
+    static string TypeFilters[MAX_LEN];    //оголошення статичного поля всередениці класу
+    static string GenreFilters[MAX_LEN];
+public:
+    Filters() {
+        type = "no type selected";
+        genre = "no type selected";
+        cout << "//Default Filters constructor//" << endl;
+    };
+    Filters(string _type, string _genre) : type(_type), genre(_genre) {
+        cout << "//Filters constructor with parameters//" << endl;
+    };
+    Filters(Filters& copy) {
+        type = copy.type;
+        genre = copy.genre;
+        cout << "//Filters copy constructor//" << endl;
+    };
+    Filters& SetType(string _type) {
+        type = _type;
+        return *this;
+    };
+    Filters& SetGenre(string _genre) {
+        genre = _genre;
+        return *this;
+    }
+    string GetType() {
+        return type;
+    };
+    string GetGenre() {
+        return genre;
+    };
+    ~Filters() {
+        cout << "//Filters destructor//" << endl;
+    };
+    void Show() {
+        cout << "Selected filters: (type) - " << type << " " << "(genre) - " << genre << endl;
+    };
+    void ShowFilters() {
+        cout << "===========================================================================================" << endl;
+        cout << "(Type) Filters: ";
+        for (int i = 0; i < MAX_TYPE_LEN; ++i) {
+            cout << TypeFilters[i] << " ";
+        }cout << endl;
+        cout << "(Genre) Filters: ";
+        for (int i = 0; i < MAX_GENRE_LEN; ++i) {
+            cout << GenreFilters[i] << " ";
+        }
+        cout << endl << "===========================================================================================";
+    };
+    // !!!тільки розуміє маленьку літеру як false
+    bool TypeSelected(const string& _type) {
+        for (int i = 0; i < MAX_TYPE_LEN; i++) {
+            if (_type == TypeFilters[i]) {
+                return true;
+            }
+        }
+        return false;
+    };
+    bool GenreSelected(const string& _genre) {
+        for (int i = 0; i < MAX_GENRE_LEN; i++) {
+            if (_genre == GenreFilters[i]) {
+                return true;  //знайшли хоч один елемент
+            }
+        }
+        return false;
+    };
+
+};
+//ініціалізація статичного поля поза класом
+string Filters::TypeFilters[] = { "Film","Series", "Cartoon" };
+string Filters::GenreFilters[] = { "Action", "Adventure", "Comedy", "Drama", "Fantasy", "Horror", "Musicals", "Romance", "Sci-fi", "Sports", "Thriller" };
+
+class Movie {
+
+};
+class MovieList {
+    vector<Movie> movie_list;
+};
+
+int main() {
+    Filters f;
+    f.ShowFilters();
+    string ggenre, ttype;
+    cout << "\nPlease select (TYPE):";
+    //cin>>ttype;
+    getline(cin, ttype);
+    cout << "Please select (GENRE):";
+    getline(cin, ggenre);
+    //cin>>ggenre;
+    Filters f_obj0;
+    Filters f_obj1(ttype, ggenre);
+    f_obj0.Show();
+    f_obj1.Show();
+
+    if (f_obj1.TypeSelected(ttype)) {
+        cout << "YES1!Selected" << endl;
+    }
+    else {
+        cout << "NO1" << endl;
+    }
+    if (f_obj1.GenreSelected(ggenre)) {
+        cout << "YES2!Selected" << endl;
+        // f_obj1.GetGenre(); // і далі щось робити з цим значенням
+    }
+    else {
+        cout << "NO2" << endl;
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
