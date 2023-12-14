088f58b63ec1091aba8ca53dbe6f2df245f96549
Marina-ai-hub <150517190+Marina-ai-hub@users.noreply.github.com>
2023-12-14 09:52:26 +0200

    Update OOP_Lab2(2)_header.h

--- OOP_Lab2(2)_header.h
+++ OOP_Lab2(2)_header.h
@@ -1,8 +1,37 @@
-//
-// Created by Настя Кіндзерська on 13.12.2023.
-//
+#ifndef UNTITLED11_LABOOP_H
+#define UNTITLED11_LABOOP_H
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <algorithm>
+#include <vector>
 
-#ifndef GROUP_21_OOP_2023_24_OOP_LAB2_2__HEADER_H
-#define GROUP_21_OOP_2023_24_OOP_LAB2_2__HEADER_H
+using namespace std;
+#define MAX_LEN 100
+#define MAX_TYPE_LEN 3
+#define MAX_GENRE_LEN 11
+#endif //UNTITLED11_LABOOP_H
 
-#endif //GROUP_21_OOP_2023_24_OOP_LAB2_2__HEADER_H
+
+class Filters {
+//    friend class Generation;
+//private:
+public:
+    string type, genre;
+    static string TypeFilters[MAX_LEN];   
+    static string GenreFilters[MAX_LEN];
+public:
+    Filters();
+    Filters(string _type, string _genre);
+    Filters(const Filters& copy);
+    Filters& SetType(string _type);
+    Filters& SetGenre(string _genre);
+    string GetType() const;
+    string GetGenre() const;
+    ~Filters();
+    void Show() const;
+    static void ShowFilters();
+    bool TypeSelected(const string& _type) const;
+    bool GenreSelected(const string& _genre) const;
+    friend istream& operator >> (istream&, Filters&);
+};
