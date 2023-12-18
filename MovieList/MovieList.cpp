a1aa79eed3a106840508163165e2704564080fbe
Marina-ai-hub <150517190+Marina-ai-hub@users.noreply.github.com>
2023-12-18 15:28:43 +0200

    Update MovieList.cpp

--- MovieList/MovieList.cpp
+++ MovieList/MovieList.cpp
@@ -6,9 +6,6 @@ void MovieList::operator()(Movie* movie) {
     movie_list.push_back(movie);
 };
 Movie* MovieList::operator[](int index) {
-    if (index < 0 || index >= movie_list.size()) {
-        throw out_of_range("Index out of range");
-    }
     return movie_list[index];
 }
 MovieList MovieList:: operator+(const MovieList& other) const {
